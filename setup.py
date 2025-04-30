7de5b37183e3774c2995fb21964de5b459755024
Kahaan Gandhi <kahaan@gmail.com>
2025-04-30 00:20:10 -0400

    add setup file colab install

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="parameterized_quantum_circuits",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "pennylane",
+        "torch",
+        "numpy",
+        "gymnasium[classic_control]",
+        "matplotlib",
+        "tqdm",
+        "pillow",
+    ],
+    python_requires=">=3.8",
+)
